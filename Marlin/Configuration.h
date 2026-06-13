7d1658228a32670858bbccd9ddb27ddac36c25b5
Maciej Szczepaniak <mszczepaniak@gmail.com>
2026-06-13 14:40:08 +0200

    fix X bed size

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1967,14 +1967,14 @@
 // @section geometry
 
 // The size of the printable area
-#define X_BED_SIZE 235
+#define X_BED_SIZE 235 - 18
 #define Y_BED_SIZE 220
 
 // Travel limits (linear=mm, rotational=°) after homing, corresponding to endstop positions.
 #define X_MIN_POS -18
 #define Y_MIN_POS 0
 #define Z_MIN_POS 0
-#define X_MAX_POS X_BED_SIZE - 18
+#define X_MAX_POS X_BED_SIZE + 18
 #define Y_MAX_POS Y_BED_SIZE
 #define Z_MAX_POS 250
 //#define I_MIN_POS 0
