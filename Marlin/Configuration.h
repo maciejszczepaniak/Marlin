1cb37c5df58a5ec6be3ca2529ebeab428eacd152
Maciej Szczepaniak <mszczepaniak@gmail.com>
2026-06-13 21:08:49 +0200

    Relax tolerances for M109 aim

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -621,8 +621,8 @@
 
 #if HAS_E_TEMP_SENSOR
   #define TEMP_RESIDENCY_TIME         10  // (seconds) Time to wait for hotend to "settle" in M109
-  #define TEMP_WINDOW                  1  // (°C) Temperature proximity for the "temperature reached" timer
-  #define TEMP_HYSTERESIS              3  // (°C) Temperature proximity considered "close enough" to the target
+  #define TEMP_WINDOW                  2  // (°C) Temperature proximity for the "temperature reached" timer
+  #define TEMP_HYSTERESIS              6  // (°C) Temperature proximity considered "close enough" to the target
 #endif
 
 #if TEMP_SENSOR_BED
