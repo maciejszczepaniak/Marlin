cc0022a3de5ae362def1b55e510fee99843cca41
Maciej Szczepaniak <mszczepaniak@gmail.com>
2026-06-27 22:38:56 +0200

    set load and unload lengths

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -945,7 +945,7 @@
  * Note: For Bowden Extruders make this large enough to allow load/unload.
  */
 #define PREVENT_LENGTHY_EXTRUDE
-#define EXTRUDE_MAXLENGTH 400
+#define EXTRUDE_MAXLENGTH 500
 
 //===========================================================================
 //======================== Thermal Runaway Protection =======================
